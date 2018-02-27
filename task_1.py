abad291617d73772fdc891c910b220abb2272295
Vilde Stokke <vildegstokke@hotmail.com>
2018-02-27 10:05:45 +0100

    change

--- task_1.py
+++ task_1.py
@@ -1,4 +1,5 @@
 from pyspark import SparkContext, SparkConf
+import tsv
 
 conf = SparkConf()
 #conf.setMaster("local")
@@ -6,11 +7,19 @@ conf = SparkConf()
 #conf.set("spark.executor.memory", "1g")
 sc = SparkContext(conf = conf)
 
-data_file = "/Users/vilde/BigData/data/geotweets.tsv"
-rdd = sc.textFile(data_file)
+#data_file = "/Users/vilde/BigData/data/geotweets.tsv"
+#rdd = sc.textFile(data_file)
 
-rdd_sample = rdd.sample(False, 0.1, 5)
+#rdd_sample = rdd.sample(False, 0.1, 5)
 
-count = rdd_sample.count()
+#count = rdd_sample.count()
 
-print("HELOOOOOO", count)
+#print("HELOOOOOO", count)
+
+
+writer = tsv.TsvWriter(open("task_1.tsv", "w"))
+
+writer.comment("This is a comment")
+writer.line("Column 1", "Column 2", 12345)
+writer.list_line(["Column 1", "Column 2"] + list(range(10)))
+writer.close()
