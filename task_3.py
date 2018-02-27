042fba447f5c679d28f38a122c08d6e377dcd60e
Vilde Stokke <vildegstokke@hotmail.com>
2018-02-27 22:58:01 +0100

    minor changes to task 3

--- task_3.py
+++ task_3.py
@@ -19,17 +19,28 @@ def createRDD(filename, val):
 def countTweets(rdd):
     countries = rdd.map(lambda x: x[1]).countByValue().items()#filter(lambda (value,count): count>= 10))
     #print(countries)
-    countries10 = list(filter(lambda (v,c): c >= 10, countries))
-    print(c)
+    countries10 = map(lambda x: x[0],list(filter(lambda (v,c): c >= 10, countries)))
+    #print("More than 10: ", countries10)
+    #print("Less than 10: ", countries)
     return countries10
+
+def filterRDD(rdd):
+    countries = countTweets(rdd)
+    rdd2 = rdd.filter(lambda x: x[1] in countries).
+    #print("Count: ", rdd2.count())
+    return rdd2
+
 
 def saveAsTextFile(filename, rdd):
         writer = tsv.TsvWriter(open(filename, "w"))
+        #Må skrives ferdig for å skrive land, latitude, longitude
+        for element in rdd:
+            writer.line()
         writer.close()
 
 def mainTask3():
     rdd = createRDD("/Users/vilde/BigData/data/geotweets.tsv", 0.1)
-    countTweets(rdd)
+    filterRDD(rdd)
     saveAsTextFile("/Users/vilde/BigData/result_3.tsv", rdd)
 
 mainTask3()
