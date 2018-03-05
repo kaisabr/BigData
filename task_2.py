4c06ee9f4340bc75bde03d976b8a76f09c830143
Vilde Stokke <vildegstokke@hotmail.com>
2018-03-05 17:06:39 +0100

    finished task 2, need to change tsv writing

--- task_2.py
+++ task_2.py
@@ -4,10 +4,8 @@ import tsv
 
 rdd = None
 rdd_sample = None
+
 conf = SparkConf()
-#conf.setMaster("local")
-#conf.setAppName("My application")
-#conf.set("spark.executor.memory", "1g")
 sc = SparkContext(conf = conf)
 
 #Initializes RDD
@@ -16,14 +14,21 @@ def createRDD(filename, val):
     rdd_sample = rdd.sample(False, val, 5)
     return rdd_sample
 
+def sortCount(rdd):
+    rddCount = rdd.map(lambda x: x[1]).countByValue().items()
+    rddSorted = sorted(rddCount, key = lambda x: (x[1]*(-1), x[0]))
+    return rddSorted
 
 
 def saveAsTextFile(filename, rdd):
         writer = tsv.TsvWriter(open(filename, "w"))
+        for country, count in rdd:
+            writer.line(country + "\t" + str(count))
         writer.close()
 
 def mainTask2():
     rdd = createRDD("/Users/vilde/BigData/data/geotweets.tsv", 0.1)
-    saveAsTextFile("/Users/vilde/BigData/result_2.tsv", rdd)
+    rddSorted = sortCount(rdd)
+    saveAsTextFile("/Users/vilde/BigData/result_2.tsv", rddSorted)
 
 mainTask2()
