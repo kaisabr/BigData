c445b08c91ea2d87fcbd8f3592c3b566964527ce
Vilde Stokke <vildegstokke@hotmail.com>
2018-03-05 17:25:08 +0100

    added comments

--- task_3.py
+++ task_3.py
@@ -24,22 +24,28 @@ def filterRDD(rdd, countries):
     return rdd10
 
 def calculateCentroid(rdd, country):
-    latitude = rdd.filter(lambda y: y[1]==country).map(lambda x: x[11]).mean()
-    longitude = rdd.filter(lambda y: y[1]==country).map(lambda x: x[12]).mean()
+    latitude = rdd.filter(lambda y: y[1]==country).map(lambda x: float(x[11])).mean()
+    longitude = rdd.filter(lambda y: y[1]==country).map(lambda x: float(x[12])).mean()
     return latitude, longitude
 
-def saveAsTextFile(filename, rdd):
-        writer = tsv.TsvWriter(open(filename, "w"))
+def saveFile(filename, rdd):
+        #writer = tsv.TsvWriter(open(filename, "w"))
         countries = countTweets(rdd)
         rddFiltered = filterRDD(rdd, countries)
         #Maa skrives ferdig for aa skrive land, latitude, longitude
+        result = []
+
         for country in countries:
             latitude, longitude = calculateCentroid(rddFiltered, country)
-            writer.line(country+"\t"+latitude+"\t"+longitude)
-        writer.close()
+            result.append((country+"\t"+str(latitude)+"\t"+str(longitude)))
+        #    writer.line(country+"\t"+str(latitude)+"\t"+str(longitude))
+        #writer.close()
+
+        resultRdd = result.parallelize(result)
+        resultRdd.coalesce(1).saveAsTextFile(filename)
 
 def mainTask3():
-    rdd = createRDD("/Users/vilde/BigData/data/geotweets.tsv", 0.1)
-    saveAsTextFile("/Users/vilde/BigData/result_3.tsv", rdd)
+    rdd = createRDD("/Users/vilde/BigData/data/geotweets.tsv", 0.001)
+    saveFile("/Users/vilde/BigData/result_3", rdd)
 
 mainTask3()
