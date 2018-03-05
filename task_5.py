e77a6aa426c2b677e72905f02e92e13216bf0f9c
Vilde Stokke <vildegstokke@hotmail.com>
2018-03-05 17:29:19 +0100

    added comments

--- task_5.py
+++ task_5.py
@@ -8,18 +8,22 @@ rdd_sample = None
 conf = SparkConf()
 sc = SparkContext(conf = conf)
 
-#Initializes RDD
+# Initializes RDD - splits on tab.
+# Val is the percentage to read from file. Default to 0.1.
 def createRDD(filename, val):
     rdd = sc.textFile(filename).map(lambda line: line.split('\t'))
     rdd_sample = rdd.sample(False, val, 5)
     return rdd_sample
 
+# Filters out all tweets not with country_code US and not place_type city
+# Counts number of tweets and sorts first by count, then by country in alphabetical order.
 def filterCityUS(rdd):
     rddFiltered = rdd.filter(lambda x: x[2]=='US' and x[3] =='city').map(lambda y: y[4])
     rddCount = rddFiltered.countByValue().items()
     rddSorted = sorted(rddCount, key = lambda x: (x[1]*(-1), x[0]))
     return rddSorted
 
+# Saves output as tsv file
 def saveAsTextFile(filename, rdd):
         writer = tsv.TsvWriter(open(filename, "w"))
         for country, count in rdd:
