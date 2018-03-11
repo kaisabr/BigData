1bbfd68fdd54225df08eefe10c34b40304358c03
kaisarokne <kaisa.rokne@gmail.com>
2018-03-11 17:47:46 +0100

    finished task 4

--- task_4.py
+++ task_4.py
@@ -1,10 +1,5 @@
 from pyspark import SparkContext, SparkConf
-import tsv
 from datetime import datetime as dt
-
-#conf.setMaster("local")
-#conf.setAppName("My application")
-#conf.set("spark.executor.memory", "1g")
 
 rdd = None
 rdd_sample = None
@@ -19,25 +14,19 @@ def createRDD(filename, val):
     rdd_sample = rdd.sample(False, val, 5)
     return rdd_sample
 
-
-def findLocalTime(rdd):
+def findMostFrequentHour(rdd):
     from collections import Counter
     # a list of timestamp objects per country:
     localTimeInSeconds = rdd.map(lambda x: (x[1], dt.fromtimestamp((float(x[0]) / 1000) + int(x[8]))))
-    #Array consisting of country name and a Counter dictionary of hour intervals and their frequency: [(country name, Counter({hour: frequency, ...})),...]
-    intervals = localTimeInSeconds.map(lambda x: (x[0], x[1].hour)).groupByKey().mapValues(lambda x: Counter(x)).collect()
-
-    output = ""
-    for x in range(0,len(intervals)):
-        #find the most frequent hour in the Counter-dictionary:
-        mostFrequentHours = intervals[x][1].most_common(1)
-        output += str(intervals[x][0]) + "\t" + str(mostFrequentHours[0][0]) + "\t" + str(mostFrequentHours[0][1]) + "\n"
-        #output = country name <tab> most frequent hour <tab> number of tweets
-    return output
+    #intervals = an array consisting of country name and a Counter dictionary of hour intervals and their frequency on the form: [(country name, Counter({hour: frequency, ...})),...]
+    #selecting hour with the highest frequency by most_common(1):
+    intervals = localTimeInSeconds.map(lambda x: (x[0], x[1].hour)).groupByKey().mapValues(lambda x: Counter(x).most_common(1)).collect()
+    mostFrequent = sc.parallelize(intervals).map(lambda (x, y): str(x) + "\t" + str(y[0][0]) + "\t"+ str(y[0][1]))
+    #mostFrequent = country name <tab> most frequent hour <tab> number of tweets
+    return mostFrequent
 
 def mainTask4():
-    rdd = createRDD("/Users/kaisarokne/git/BigData/geotweets.tsv", 0.0001)
-    print findLocalTime(rdd)
-    #saveAsTextFile("/Users/kaisarokne/git/BigData/result_4.tsv", rdd, df)
-
+    rdd = createRDD("./geotweets.tsv", 1)
+    mostFrequentHour = findMostFrequentHour(rdd)
+    mostFrequentHour.coalesce(1).saveAsTextFile("./result_4.tsv")
 mainTask4()
