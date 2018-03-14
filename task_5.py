f50fa5968d35cd61c5f61be79b7c9477d64b7e9a
Vilde Stokke <vildegstokke@hotmail.com>
2018-03-14 14:26:36 +0100

    added comments

--- task_5.py
+++ task_5.py
@@ -1,23 +1,24 @@
 from pyspark import SparkContext, SparkConf
-import tsv
 
-#Initializes SparkContext and SparkConf
+#Initializes spark config and context
 rdd = None
 rdd_sample = None
 
 conf = SparkConf()
 sc = SparkContext(conf = conf)
 
-# Initializes RDD - splits on tab.
-# Val is the percentage to read from file. Default to 0.1.
+#Initializes RDD - loading the dataset into an RDD using textFile
+#Maps each line in the dataset so that it splits on tab
+#Samples "val" percentage of that RDD (e.g. val = 0.1 --> 10% of dataset)
 def createRDD(filename, val):
     rdd = sc.textFile(filename, use_unicode=True).map(lambda line: line.split('\t'))
     rdd_sample = rdd.sample(False, val, 5)
     return rdd_sample
 
-# Filters out all tweets not with country_code US and not place_type city
-# Counts number of tweets and sorts first by count, then by country in alphabetical order.
-# Creates rdd with city name <tab> count.
+#Filters out all tweets not with country_code US (column 2) and not place_type city (column 3)
+#Maps so that we're left with column 4 - place_name
+#Counts number of tweets by value (place name) and sorts first by count, then by country in alphabetical order.
+#Creates RDD with city name <tab> count using parallelize.
 def countCityUS(rdd):
     rddFiltered = rdd.filter(lambda x: x[2]=='US' and x[3] =='city').map(lambda y: y[4])
     rddCount = rddFiltered.countByValue().items()
